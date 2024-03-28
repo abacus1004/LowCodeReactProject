b14c3778fc1248b160051aa881c61cfb5a50e3f4
Manohar Angirekula <manohar@MacBook-Pro-2.local>
2024-03-28 20:16:04 +0530

    using json object

--- src/monacoEditor/MonacoEditorComponent.jsx
+++ src/monacoEditor/MonacoEditorComponent.jsx
@@ -5,59 +5,50 @@ import * as monaco from 'monaco-editor'
 
 function MonacoEditorComponent() {
 
-  const functionCoreSuggestions = [
-    {
-      label: 'getCookie',
-      kind: monaco.languages.CompletionItemKind.Keyword,
-      insertText: 'getCookie',
-    },
-    {
-      label: 'sleep',
-      kind: monaco.languages.CompletionItemKind.Keyword,
-      insertText: 'sleep',
-    },
-    {
-      label: 'setCookie',
-      kind: monaco.languages.CompletionItemKind.Keyword,
-      insertText: 'setCookie',
-    },
-    {
-      label: 'goToDivId',
-      kind: monaco.languages.CompletionItemKind.Keyword,
-      insertText: 'goToDivId',
-    }
-  ]
+  function generateTypeDeclaration(obj, objectName = 'myObject') {
+    let typeDeclaration = `declare var ${objectName}: {\n`;
 
-  const functionSuggestions = [
-    {
-      label: 'core',
-      kind: monaco.languages.CompletionItemKind.Variable,
-      insertText: 'core',
+    for (const key in obj) {
+        if (typeof obj[key] === 'object') {
+            // If the property is an object, recursively generate the type declaration for it
+            typeDeclaration += `${key}: {\n`;
+            const nestedObj = obj[key];
+            for (const nestedKey in nestedObj) {
+                typeDeclaration += `${nestedKey}: ${typeof nestedObj[nestedKey]};\n`;
+            }
+            typeDeclaration += '};\n';
+        } else {
+            // If the property is not an object, simply declare its type
+            typeDeclaration += `${key}: ${typeof obj[key]};\n`;
+        }
     }
-  ]
 
-  const provideCustomSuggestions = (model, position) => {
-    var lineContent = model.getLineContent(position.lineNumber);
+    typeDeclaration += '};\n';
+    alert(typeDeclaration);
+    return typeDeclaration;
+}
 
-    if(lineContent === 'functions.core.'){
-      return { suggestions: functionCoreSuggestions };
-    }
-    else if(lineContent === 'functions.'){
-      return { suggestions: functionSuggestions };
-    }
-  };
-  
-  const handleEditorDidMount = (editor, monaco) => {
-    monaco.languages.registerCompletionItemProvider('javascript', {
-      provideCompletionItems: provideCustomSuggestions,
-      triggerCharacters: ["."]
-    });
-  };
+const myObject = {
+  name: "John",
+  age: 30,
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    country: "USA"
+  },
+  greet: function() {
+  },
+  getLocation: function() {}
+};
+
+const typeDeclaration = generateTypeDeclaration(myObject);
 
   return <Editor height="90vh"
                  language="javascript"
                  defaultValue="// Type Code here"
-                 onMount = {handleEditorDidMount}
+                 beforeMount={(monaco) => {
+                  monaco.languages.typescript.javascriptDefaults.addExtraLib(typeDeclaration, 'filename/typeDeclaration.d.ts');
+              }}
         />;
 };
 
